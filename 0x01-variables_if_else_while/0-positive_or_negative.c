5029448b8bae7e8f19347fe43f053df39b40f0ca
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-17 09:26:34 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main -> Entery Point
  *
@@ -10,8 +11,7 @@
  */
 int main(void)
 {
-	int n;
-	
+	int n;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
