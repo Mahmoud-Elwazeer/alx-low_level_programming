4ea49715f7bcf24817c22af192b315b6b9540136
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-17 14:03:19 -0700

    adv

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -> Entry Point
+ *
+ * Objective: Use putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if ((i == '9') && (j == '9'))
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
