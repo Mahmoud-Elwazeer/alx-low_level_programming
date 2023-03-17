8c1808991a81b19dfa98eaac40e9b89b5f2b7483
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-16 17:09:53 -0700

    without printf & puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <sydio.h>
+#include <stdio.h>
 
 /**
  * main -> Entry point
@@ -7,5 +7,6 @@
  */
 int main(void)
 {
-	
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);	
 }
