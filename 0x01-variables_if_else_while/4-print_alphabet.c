a3511f9babf3685cbbd0d791396b3cd21028619a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-17 12:46:14 -0700

    for

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -> Entry Point
+ *
+ * Objective: Use putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if ((i == 'q') || (i == 'e'))
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
