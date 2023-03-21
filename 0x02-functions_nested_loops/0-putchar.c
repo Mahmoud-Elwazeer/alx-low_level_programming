f3655ac127c8a83506f881e9f9006b4149a89c88
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-21 14:06:10 -0700

    func_print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void printword(void);
+
+/**
+ * main -> Entry Point
+ * 
+ * Return -> Always 0 (Success)
+ */
+int main(void)
+{
+	printword();
+	return (0);
+}
+
+/**
+ * printword -> print(_putchar)
+ *
+ * No Return
+ */
+void printword(void)
+{
+	printf("_putchar\n");
+}
