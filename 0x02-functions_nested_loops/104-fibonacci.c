690f120b6ebc2d1da0c9e5518fc8decf41b9d679
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-22 15:42:32 -0700

    adv

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	long f1 = 1, f2 = 2, f3;
+	int i;
+
+	printf("%ld, %ld", f1, f2);
+	for (i = 0; i < 96; i++)
+	{
+		f3 = f1 + f2;
+		f1 = f2;
+		f2 = f3;
+		printf(", %ld", f3);
+	}
+	printf("\n");
+	return (0);
+}
