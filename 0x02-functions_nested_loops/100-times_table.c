dcc4677c88c4ab159fff2ac041e2e4cb7786ddde
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-21 19:25:16 -0700

    func

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table -  prints the 9 times table, starting with 0
+ * @n: number
+ */
+void print_times_table(int n)
+{
+	int i, j, check;
+
+	if (n >= 0 || n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				check = i * j;
+				if (j == 0)
+				{
+					_putchar('0');
+					_putchar(',');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					if (check > 99)
+					{
+						_putchar(check / 100 + '0');
+						_putchar(check / 10 + '0');
+					}
+					else if (check > 9)
+					{
+						_putchar(' ');
+						_putchar(check / 10 + '0');
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(check % 10 + '0');
+					if (j == 9)
+						continue;
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
