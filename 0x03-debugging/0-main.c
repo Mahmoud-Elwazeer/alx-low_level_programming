d56fb2a7f2603e9ab261ff1a72fc90a2c13896d0
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-22 16:21:51 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,12 +10,7 @@ int main(void)
         int i;
 
         i = 98;
-        if (positive_or_negative(i) == 1)
-		printf("%d is positive", i);
-	else if (positive_or_negative(i) == 0)
-		printf("%d is zero", i);
-	else
-		printf("%d is negative", i);
+        positive_or_negative(i);
 
         return (0);
 }
