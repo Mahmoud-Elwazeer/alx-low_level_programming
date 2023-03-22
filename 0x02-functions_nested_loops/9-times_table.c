e16d24249726d23ce70609e9ea5e180dd294f682
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-21 18:24:22 -0700

    func

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,15 +5,32 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, check;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i*j + '0');
-			_putchar(',');
-			_putchar('\t');
+			check = i * j;
+			if (check < 10)
+			{
+				_putchar(check + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(check / 10 + '0');
+				_putchar(check % 10 + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+			}
+			if (j == 9)
+				continue;
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
