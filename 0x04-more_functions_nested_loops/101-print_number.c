cf947db26225767bda16dd65748c276aa8f4e53c
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-23 09:54:26 -0700

    adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <math.h>
 /**
  * print_number - print integer
  * @n: Number to Print
@@ -7,6 +7,31 @@
  */
 void print_number(int n)
 {
+	int count = 0, calc = n;
+	int flag = 1;
+	int p;
 	if (n == 0)
 		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			flag = -1;
+			n = -n;
+		}
+		while (calc > 0)
+		{
+			calc = calc / 10;
+			count++;
+		}
+		if (flag == -1)
+			_putchar('-');
+		while (n > 0)
+		{
+			count--;
+			p = n / pow(10, count);
+			n = n - pow(10, count) * j;
+			_putchar(p + '0');
+		}
+	}
 }
