7d0404289c41ce71b828eabf3a745f2d9e5ff88d
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-24 11:02:20 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,24 @@ void print_number(int n)
 {
 	int count = 0, calc = n;
 	int rem = 1, p;
-	int i;
+	int i, flag = 1;
 
 	if (n == 0)
 		_putchar('0');
 	else
 	{
+		if (n < 0)
+		{
+			flag = -1;
+			n = -n
+		}
 		while (calc > 0)
 		{
 			calc = calc / 10;
 			count++;
 		}
+		if (flag = -1)
+			_putchar('-');
 		while (count > 0)
 		{
 			for (i = 1; i <= count; i++)
