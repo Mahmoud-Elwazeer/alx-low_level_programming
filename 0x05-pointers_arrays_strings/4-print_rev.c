65450ba9ed28b8a161edf0c1a364c7d692f4b31c
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-27 08:29:43 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,10 @@ void print_rev(char *s)
 	int count;
 
 	for (count = 0; (*(s + count)) != '\0'; count++);
-	while (count < 0)
+	while (count > 0)
 	{
 		count--;
-		_putchar (*(s + count));
+		_putchar(*(s + count));
 	}
 }
 
