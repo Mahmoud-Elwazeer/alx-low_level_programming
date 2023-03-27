89bf2cab89fb76d0ce115bbbca960f4d61e19873
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-27 11:12:09 -0700

    adv

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,9 @@ int _atoi(char *s)
 			flag = -1;
 		else if (*(s + i) == '+')
 			flag = 1;
-		if ((*(s + i) >= 'a' && *(s + i) <= 'z') || (*(s + i) >= 'A' && *(s + i) <= 'Z'))
+		if ((*(s + i) >= 'a' && *(s + i) <= 'z'))
+			continue;
+		if ((*(s + i) >= 'A' && *(s + i) <= 'Z'))
 			continue;
 		if (*(s + i) >= '0' && *(s + i) <= '9')
 			r = *(s + i) - '0';
