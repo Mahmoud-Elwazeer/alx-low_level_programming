ca5c107ab47cc23d2da0ef3b0519f975880b0455
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-27 11:08:52 -0700

    adv

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,3 +4,22 @@
  * _atoi - convert a string to integer
  * @s: pointer point to start string
  * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, r;
+	char sign = 1;
+
+	for (i = 0; (*(s + i)) != '\0'; i++)
+	{
+		if (*(s + i) == '-')
+			flag = -1;
+		else if (*(s + i) == '+')
+			flag = 1;
+		if ((*(s + i) >= 'a' && *(s + i) <= 'z') || (*(s + i) >= 'A' && *(s + i) <= 'Z'))
+			continue;
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+			r = *(s + i) - '0';
+	}
+	return (r * flag);
+}
