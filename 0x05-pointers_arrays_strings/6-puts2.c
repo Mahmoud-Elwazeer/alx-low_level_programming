01da8943b223eb4fde95d800d48453a652aa2583
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-27 09:02:18 -0700

    even char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print even charcter of string
+ * @str: pointer
+ * Return: void
+ */
+void puts(char *str)
+{
+	int count = 0;
+
+	while ((*(str + count)) != '\0')
+	{
+		if (count % 2 == 0)
+			_putchar(*(str + count));
+		count++;
+	}
+	_putchar('\n');
+}
