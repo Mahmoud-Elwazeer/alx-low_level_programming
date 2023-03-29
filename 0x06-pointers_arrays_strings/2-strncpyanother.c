e0a6421d87a0d5688177507698233d8a1bd7c1d4
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-29 08:36:52 -0700

    effiviet code

--- 0x06-pointers_arrays_strings/2-strncpyanother.c
+++ 0x06-pointers_arrays_strings/2-strncpyanother.c
@@ -9,8 +9,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	static char r[1000];
 	int i, size = 0;
 
+	for (i = 0; *(dest + i) != '\0'; i++)
+		*(r + i) = *(dest + i);
+	*(r + i) = '\0';
 	for (i = 0; *(src + i) != '\0'; i++)
 		size++;
 	for (i = 0; i < n; i++)
@@ -18,10 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 		if (i < size)
 		{
 			*(dest + i) = *(src + i);
+			*(r + i) = *(src + i);
 		}
 		else
 		{
 			*(dest + i) = '\0';
+			*(r + i) = '\0';
 		}
 	}
 	return (dest);
