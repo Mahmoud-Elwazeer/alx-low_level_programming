12f3128a800d44f00e48692058785860078314d9
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-03-29 16:40:18 -0700

    ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: pointer to string
+ * Return: encoder string
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'm')
+			*(s + i) += 13;
+		else if (*(s + i) >= 'A' && *(s + i) <= 'M')
+			*(s + i) += 13;
+		else if (*(s + i) >= 'n' && *(s + i) <= 'z')
+			*(s + i) -= 13;
+		else if (*(s + i) >= 'N' && *(s + i) <= 'Z')
+			*(s + i) -= 13;
+	}
+	return (s);
+}
