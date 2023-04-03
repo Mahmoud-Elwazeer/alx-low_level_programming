8fb2eaf3abc2accd851b9da76c7641b15fdf4607
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-03 14:23:53 -0700

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,19 +6,22 @@
  * @a: matrix
  * @size: size of matrix
  * Return: void
+ * note: *(ptr + i * COLS + j)
  */
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int sum1 = 0;
+	int sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (j == i)
-				sum1 += a[j];
+				sum1 += *(a + i * size + j);
+			if (j == size - 1 - i)
+				sum2 += *(a + i * size + j);
 		}
 	}
-	printf("%d\n" , sum1);
+	printf("%d, %d\n" , sum1, sum2);
 }
