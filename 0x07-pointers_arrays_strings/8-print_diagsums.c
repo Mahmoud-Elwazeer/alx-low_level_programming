648e731f60c8536afb96700cff3986af1f98d89f
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-03 14:27:20 -0700

    test betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums : prints the sum of the two diagonals of matrix
+ * print_diagsums - prints the sum of the two diagonals of matrix
  * @a: matrix
  * @size: size of matrix
  * Return: void
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 	int i, j;
 	int sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for(i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
