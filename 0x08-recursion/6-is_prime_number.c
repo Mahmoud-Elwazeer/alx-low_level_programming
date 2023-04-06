5382467a84c1e25a54531c1ff10eda853708702d
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-05 17:33:28 -0700

    primer number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number_aux - auxuiliary function
+ * @n: Tne Number to check
+ * @i: index
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number_aux(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return (is_prime_number_aux(n, i + 1));
+}
+
+/**
+ * is_prime_number - prime number checker
+ * @n: Number to check
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number_aux(n, 2));
+}
