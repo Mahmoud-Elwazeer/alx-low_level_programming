976d536698ebbb64b344dd18c076e6555a138ce3
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-08 14:46:55 -0700

    print the result of the multiplication,

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry func
+ * @argc: Number of commands
+ * @argv: array include commands
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	
+	if (argc > 1)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
