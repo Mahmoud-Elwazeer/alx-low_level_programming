c893d4d4cf218d6352570cd9ec3c122497822af5
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-08 14:26:09 -0700

    prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry func
+ * @agrc: Number of commands
+ * @agrv: array include commands
+ * Return: Always 0
+ */
+int main(int agrc, char __attribute__ ((unused)) *agrv[])
+{
+	printf("%d\n", agrc);
+	return (0);
+}
