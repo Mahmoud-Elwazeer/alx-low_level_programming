8eece3d8ab20bdb2fcf5e586d35d7f5912174a3a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-08 14:12:28 -0700

    print the name of programme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry func
+ * @argc: Number of command lines
+ * @argv: array for content command lines
+ * Return: Always 1
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
