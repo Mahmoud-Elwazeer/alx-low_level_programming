8fe8ba6d1bbebf6588e83df8ac157bb9c46e837a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-08 14:19:26 -0700

    fix error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
  * main - Entry func
  * @argc: Number of command lines
  * @argv: array for content command lines
- * Return: Always 1
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
