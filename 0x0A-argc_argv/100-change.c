d1512e1d6d8bd539a0a6b1720cdbac91b714f296
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-10 12:58:42 -0700

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int n = 0, flag = 0;
+	int n = 0, flag = 0, num = 0;
 	int i = 0, j = 0, count = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
@@ -22,12 +22,13 @@ int main(int argc, char *argv[])
 				j = 1;
 				while (coins[i] * j <= n)
 				{
+					num = coins[i] * j;
 					flag = 1;
 					j++;
 				}
 				if (flag)
 				{
-					n -= coins[i] * --j;
+					n -= num;
 					count += --j;
 					flag = 0;
 				}
