194b5460c1408c8d9fe105fc8333faf23b664f96
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-11 11:00:41 -0700

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
  * _strdup - a pointer to a newly allocated space in memory, 
  * which contains a copy of the string given as a parameter.
  * @str: pointer to string
- * Return:  a pointer to a new string which is a duplicate of the string str
+ * Return: a pointer to a new string which is a duplicate of the string str
  */
 char *_strdup(char *str)
 {
