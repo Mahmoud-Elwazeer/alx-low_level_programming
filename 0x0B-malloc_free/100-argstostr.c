ac075542bd6b202e377e42a4541b6c5a553d9f18
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-11 18:16:46 -0700

    test

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: argc
+ * @av: argv
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	return (NULL);
+}
