ec2dac70e958caaf9bd76d14e665ff100b43478f
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-13 15:03:59 -0700

    test doc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
+ * malloc_checked : allocates memory using malloc
  * @b: size of memory
- * Return: a pointer to the allocated memory
+ * Return: pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
