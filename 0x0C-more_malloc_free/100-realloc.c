a25baaa4e0599d81fd04130123ca5eb5e84e0a61
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-14 16:28:58 -0700

    test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 		else
 		{
-			ptr = old_size + malloc(new_size - old_size);
+			ptr = malloc(new_size);
 			if (ptr == NULL)
 				return (NULL);
 			return (ptr);
