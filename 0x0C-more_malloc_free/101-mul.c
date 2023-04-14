18c56e5dfdb7396f2cfee5652994d9a725457771
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-14 16:52:08 -0700

    test

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of input
+ * @argv: array of input
+ * Return: Always 0 when success
+ */
+int main(int argc, char *argv[])
+{
+	long mul, num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%ld\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
