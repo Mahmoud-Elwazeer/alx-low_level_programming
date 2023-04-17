d83eab5b6112d1a2daf13c5b317cafdffc306492
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-17 07:35:30 -0700

    test betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * init_dog : function that initialize a variable of type struct dog
+ * init_dog - function that initialize a variable of type struct dog
  * @d: pointer to struct
  * @name: Name of dog
  * @age: age of dog
@@ -10,6 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
 	d->name = name;
 	d->age = age;
 	d-> owner = owner;
