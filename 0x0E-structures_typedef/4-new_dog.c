6a41f955db1ce404f52662e7e473125ec68cab46
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-17 08:52:02 -0700

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,15 +17,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 	for(size_owner = 0; *(owner + size_owner) != '\0'; size_owner++)
 		;
+
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+
 	d->name = (char *)malloc(sizeof(size_name + 1));
 	if (d->name == NULL)
 		return (NULL);
+
 	d->owner = (char *)malloc(sizeof(size_owner + 1));
 	if (d->owner == NULL)
 		return (NULL);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
