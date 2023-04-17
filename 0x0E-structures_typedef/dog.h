cb30373620d622ea524c15129f211430c1d25d20
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-17 07:31:08 -0700

    add doc to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: pointer to struct
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: void
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
