f275a1a550f02be8342c17f4b60062e6fd036f91
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-19 15:28:35 -0700

    test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of input
+ * @argv: Array of content
+ * Return: always 0 when success
+ */
+int main(int argc, char *argv[])
+{
+	int calc, num1, num2;
+	char operator;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	calc = (*get_op_func(&operator))(num1, num2);
+
+	printf("%d\n", calc);
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - summ two numbers
+ * @a: 1st Number
+ * @b: 2nd Number
+ * Return: sum two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two numbers
+ * @a: 1st Number
+ * @b: 2nd Number
+ * Return: Sub two number
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul two numbers
+ * @a: 1st Number
+ * @b: 2nd Number
+ * Return: mul two number
+ */
+int op_mul(int a, int b)
+{
+	return ((long)a * b);
+}
+
+/**
+ * op_div - div two numbers
+ * @a: 1st Number
+ * @b: 2nd Number
+ * Return: div two number
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod two numbers
+ * @a: 1st Number
+ * @b: 2nd Number
+ * Return: mod two number
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
