63b24f797bb22f0cf377b18b66f8aae6de7cd73e
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-20 14:03:16 -0700

    fix

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -45,8 +45,6 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
+	print_name("Hodor", NULL);
+	return (0);
 }
