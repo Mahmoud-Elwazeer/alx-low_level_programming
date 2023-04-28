892b14b9e105f02fe3e808a89552844ea41115d4
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-27 17:26:49 -0700

    test ftest free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,4 +2,12 @@
 #include <stdlib.h>
 
 /**
- *
+ * free_list - frees a list_t list.
+ * @head: pointer to struct
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	free(head->str);
+	free(head);
+}
