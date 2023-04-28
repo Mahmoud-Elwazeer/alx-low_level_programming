96c7c7ec9687ea0244160c7c44c646be38a93bbd
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-04-27 17:11:34 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * add_node -  adds a new node at the beginning of a list_t list.
+ * add_node_end -  adds a new node at the beginning of a list_t list.
  * @head: pointer to pointer of beginning
  * @str: value of node
  * Return: address of new node
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *last;
