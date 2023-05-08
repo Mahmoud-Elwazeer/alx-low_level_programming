4d0f74abe664625e0a54fb0b1670f2b9b7c1332a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-08 16:32:37 -0700

    fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * create_file - create file
  * @filename: name of file
  * @text_content: content of file
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -23,9 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (write(fd, text_content, size) != size)
+	if (write(fd, text_content, size) == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 
