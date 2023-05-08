f237c2185ffd109a31741e015f52d4d3a373ccaa
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-08 16:03:22 -0700

    fix betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 /**
  * read_textfile -  reads a text file and prints it
  * @filename: pointer to filename
- * @letteres: letters is the number of letters it should read and print
+ * @letters: letters is the number of letters it should read and print
  * Return: the actual number of letters it could read and print
  * Or 0 if fail
  */
@@ -17,10 +17,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	
-	if ((fd = open(filename, O_RDONLY)) == -1)
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	
+
 	ch = (char *)malloc(sizeof(char) * letters);
 	if (ch == NULL)
 		return (0);
@@ -31,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if ((write(STDOUT_FILENO, ch, read_byte)) == -1)
 			return (0);
 	}
-	
+
 	close(fd);
 	free(ch);
 	return (size++);
