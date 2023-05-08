9fd5c68fab6f870e257eddb2752aa265d6ac3aca
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-08 16:29:46 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - create file
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: 1 on success, -1 on failure 
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int size = 0;
+
+	for (size = 0; *(text_content + size) != '\0'; size++)
+		;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (write(fd, text_content, size) != size)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
