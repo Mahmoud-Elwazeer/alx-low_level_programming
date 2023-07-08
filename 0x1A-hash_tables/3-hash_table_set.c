77795b8a263a394bf958923419b8618a810754f0
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-07 17:32:13 -0700

    test

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - t adds an element to the hash table.
+ * @ht: hash_table
+ * @key: key
+ * @value: value
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	return (1);
+}
