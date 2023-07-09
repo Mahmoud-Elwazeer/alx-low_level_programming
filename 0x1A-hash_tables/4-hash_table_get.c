8eee8587f5273d0499e8804fc86c2f89393c8c52
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-09 11:11:34 -0700

    get element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key.
+ * @ht: hash table you want to look into
+ * @key: the key you are looking for
+ *
+ * Return: the value associated with the element, or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index = key_index((unsigned const char *)key, ht->size);
+	hash_node_t *temp = ht->array[index];
+
+	if (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+	}
+	return (NULL);
+}
