1ef18cafda5b002993b41155984f85957aa71756
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-09 10:29:00 -0700

    create node in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,5 +9,49 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *temp = NULL;
+	hash_node_t *current_node = NULL;
+
+	if (strcmp(key, "") == 0)
+		return (1);
+	temp = create_node(key, value);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (temp == NULL)
+		return (1);
+
+	current_node = ht->array[index];
+	if (current_node == NULL)
+	{
+		ht->array[index] = temp;
+		return (0);
+	}
+	else
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			strcpy(ht->array[index]->value, value);
+			return (0);
+		}
+	}
 	return (1);
+}
+
+/**
+ * create_node - create new item in hash
+ * @key: key
+ * @vakue: value
+ *
+ * Return: new node
+ */
+hash_node_t *create_node(const char* key, const char* value)
+{
+	hash_node_t *temp = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+	if (temp == NULL)
+		return (NULL);
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+
+	return (temp);
 }
