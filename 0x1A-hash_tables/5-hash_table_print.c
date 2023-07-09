3463bd66061cb86ca9fd1ace1abd42357c168c64
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-07-09 11:40:26 -0700

    test

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -  prints a hash table.
+ * @ht: hash_table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (i == ht->size - 2)
+				continue;
+			else
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
