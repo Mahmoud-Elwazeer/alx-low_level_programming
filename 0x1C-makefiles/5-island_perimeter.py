33619b9ac06f5e7ac49e8073aac49ee23a434a0a
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-23 11:33:10 -0700

    calc perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,4 +2,25 @@
 """island_perimeter"""
 
 def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    length = 0
+    width = 0
 
+    for i in grid:
+        if length == 0:
+            length = sum(i)
+        if length < sum(i):
+            length = sum(i)
+
+    for i in range(len(grid)):
+        check = 0
+        for j in range(len(grid)):
+            check += grid[j][i]
+
+        if check > 1:
+            width = check
+            break
+        elif check == 1:
+            width = check
+
+    return ((length + width) * 2)
